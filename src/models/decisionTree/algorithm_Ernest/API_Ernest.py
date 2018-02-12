8da5ee3405e1a3e984cca67c6d7572e4b6c14388
moon <moonsin@outlook.com>
2018-02-12 02:13:06 +0000

    Preliminary decision tree algorithm

--- src/models/decisionTree/algorithm_Ernest/API_Ernest.py
+++ src/models/decisionTree/algorithm_Ernest/API_Ernest.py
@@ -21,11 +21,12 @@ def getGainDicByFeature(dataSet, feature, entDValue, entD_feature) :
     entList = []
     entSum = 0
     sum = 0
+    IV = 0
 
     # Use dichotomy to handle continuous discrete attributes
     # Judge whether it is a continuous value
-    if( len(valuesRateList) >=15 and str(valuesList[0]).isdigit()) :
-        dict = _getGainDicByFeatureInContinuous(dataSet, featur, entDValue, entD_feature )
+    if( len(valuesRateList) >=15 and str(valuesList[valuesList.index[0]]).isdigit()) :
+        dict = _getGainDicByFeatureInContinuous(dataSet, feature, entDValue, entD_feature )
         return dict
     else :
         for i in range(num) :
@@ -42,16 +43,33 @@ def getGainDicByFeature(dataSet, feature, entDValue, entD_feature) :
 
 
 def attrSetGenerate (dataSet) :
+    #print(dataSet)
+    #print('current Function: attrSetGenerate') 
+
     attrSet = {}
     ent_attrition = entD(dataSet, 'Attrition')
     for index in dataSet.columns :
-        attrSet[index] = []
-        valuesList = dataSet[index]
-        valuesRateList = dataSet[index].value_counts(normalize=True)
-        if( len(valuesRateList) >=15 and str(valuesList[0]).isdigit()) :
-            dict = _getGainDicByFeatureInContinuous(dataSet, index, ent_attrition, 'Attrition')
-            print(dict)
+        if index!='Attrition' :
+            #print('current Feature: ',index)
+            
+            attrSet[index] = []
+            valuesList = dataSet[index]
+            valuesRateList = dataSet[index].value_counts(normalize=True)
+            num = len(valuesRateList)
+            #print(valuesList.index[0])
+            #Judge whether it is a continuous value
+            if( len(valuesRateList) >=15 and str(valuesList[valuesList.index[0]]).isdigit()) :
+                #dict = _getGainDicByFeatureInContinuous(dataSet, index, ent_attrition, 'Attrition')
+                #attrSet[index] = {'bestBoundary': dict['bestBoundary']} 
+                attrSet[index] = {'ifContinuous':True}
+
+            else :
+                for i in range(num) :
+                    attrSet[index].append(valuesRateList.index[i])
+                    #print(valuesRateList.index[i])
+
     #print(attrSet)
+    return attrSet
     
 
 def _getGainDicByFeatureInContinuous (dataSet, feature, entDValue, entD_feature) :
@@ -61,6 +79,8 @@ def _getGainDicByFeatureInContinuous (dataSet, feature, entDValue, entD_feature)
 
     bestBoundary = ( orderedValuesList[0] + orderedValuesList[1] ) / 2
     bestGain = 0
+    IV = 0
+    bestI = 0
 
     ListNum = len(dataSet)
 
@@ -86,6 +106,42 @@ def _getGainDicByFeatureInContinuous (dataSet, feature, entDValue, entD_feature)
             IV = -(P* math.log2(P) + (1-P) * math.log2(1 - P))
 
     return {'gain': bestGain, 'IV':IV, 'continuous': True, 'bestBoundary': bestBoundary, 'bestI':bestI}
+
+
+def ifSameClass(dataSet, feature) :
+
+    valuesRateList = dataSet[feature].value_counts(normalize=True)
+
+    if (len(valuesRateList) == 1):
+        return valuesRateList.index[0]
+    else :
+        return 0
+
+def ifAllClassSame(dataSet) :
+    df2 = dataSet.copy()
+    del df2['Attrition']
+    for index in df2.columns :
+        values = df2[index].value_counts(normalize=True)
+        if(len(values) != 1) :
+            return 0 
+
+    return 1
+
+def getClass(dataSet, predictfeature) :
+     
+    bestClass = ''
+    bestRate = 0
+    valuesRateList = dataSet[predictfeature].value_counts(normalize=True)
+    
+    for i in range(len(valuesRateList)) :
+        if valuesRateList[valuesRateList.index[i]] > bestRate :
+            bestClass = valuesRateList.index[i]
+            bestRate = valuesRateList[valuesRateList.index[i]]
+
+    return bestClass
+
+
+
 
 
 
--- src/models/decisionTree/algorithm_Ernest/decisionTree.py
+++ src/models/decisionTree/algorithm_Ernest/decisionTree.py
@@ -4,14 +4,102 @@ import csv
 import pandas as pd
 import math
 import API_Ernest
+from collections import defaultdict
 
 filename = './data/data_Ernest/entireDataSet_Ernest.csv'
 dataSet = pd.read_csv(filename)
-atrrSet = {}
+entireDataSet = dataSet.copy()
+predictfeature = 'Attrition'
+entireAttrSet = API_Ernest.attrSetGenerate(dataSet)
 
-API_Ernest.attrSetGenerate(dataSet)
+#def tree(): return defaultdict(tree)
 
-#def treeGenerate(dataSet, atrrSet) :
+decisionTree = {}
+
+def treeGenerate(dataSet, decisionTree) :
+    print('create new node')
+    ent_attrition = API_Ernest.entD(dataSet, 'Attrition')
+    global entireAttrSet
+    dataSet = dataSet.reset_index(drop=True) 
+    currentAttrSet = API_Ernest.attrSetGenerate(dataSet)
+    #print(currentAttrSet)
+    ifSame = API_Ernest.ifSameClass(dataSet, predictfeature)
+    if ifSame :
+        decisionTree[predictfeature] = ifSame 
+        return
+
+    if (len(currentAttrSet) == 0 or API_Ernest.ifAllClassSame(dataSet)) :
+
+        featureClass = API_Ernest.getClass(dataSet, predictfeature)
+        decisionTree[predictfeature] = featureClass
+
+    #compute every feature and get best one
+    gain = 0
+    gain_ratio = 0
+    bestFeature = ''
+    bestBoundary = 0
+    #print(dataSet)
+    #print('Current Function: find best feature')
+    for index in dataSet.columns :
+        if index != 'Attrition':
+            gainDic_feature = API_Ernest.getGainDicByFeature(dataSet, index, ent_attrition, 'Attrition')
+            print('Current feature', index)
+            print('Gain',gainDic_feature['gain'])
+
+
+            if gainDic_feature['gain'] > gain :
+                gain = gainDic_feature['gain']
+                bestFeature = index
+                if gainDic_feature['continuous'] :
+                    bestBoundary = gainDic_feature['bestBoundary'] 
+    decisionTree[bestFeature] = {}
+    
+    if (type(entireAttrSet[bestFeature]) == dict) :
+            decisionTree[bestFeature]['>='+ str(bestBoundary)]= {}
+            decisionTree[bestFeature]['<'+ str(bestBoundary)]= {}
+            subDataBig = dataSet.loc[dataSet[bestFeature] >= bestBoundary ]
+            subDataSmall = dataSet.loc[dataSet[bestFeature] < bestBoundary ]
+            del subDataBig[bestFeature]
+            del subDataSmall[bestFeature]
+         
+            if (len(subDataBig) == 0) :
+                featureClass = API_Ernest.getClass(dataSet, predictfeature)
+                decisionTree[bestFeature]['>='+ str(bestBoundary)][predictfeature] = featureClass
+            else :
+                treeGenerate(subDataBig, decisionTree[bestFeature]['>='+ str(bestBoundary)])
+            if (len(subDataSmall) == 0) :
+                featureClass = API_Ernest.getClass(dataSet, predictfeature)
+                decisionTree[bestFeature]['<'+ str(bestBoundary)][predictfeature] = featureClass
+            else :
+                treeGenerate(subDataSmall, decisionTree[bestFeature]['<'+ str(bestBoundary)])
+
+
+
+    else :
+        for index in entireAttrSet[bestFeature] :
+            decisionTree[bestFeature][index] = {}
+            subData = dataSet.loc[dataSet[bestFeature] == index ]
+            del subData[bestFeature]
+            
+            if (len(subData) == 0) :
+
+                featureClass = API_Ernest.getClass(dataSet, predictfeature)
+                decisionTree[bestFeature][index][predictfeature] = featureClass
+            else :
+                treeGenerate(subData, decisionTree[bestFeature][index])
+    return decisionTree
+    
+decisionTree = treeGenerate(dataSet, decisionTree)
+print(decisionTree)
+#print(entireAttrSet);
+#print(type(entireAttrSet['BusinessTravel']) )
+#print(dataSet.loc[dataSet['MonthlyRate'] <=100000]) 
+#print(dataSet['Department'])
+
+
+
+
+
 
 
 
