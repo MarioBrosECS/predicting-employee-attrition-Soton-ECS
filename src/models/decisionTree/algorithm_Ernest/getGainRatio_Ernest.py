f67be6da08f90d370c847e64906640819f71bf9a
moon <moonsin@outlook.com>
2018-02-19 22:54:44 +0000

    add information gain reference and regulazation

--- src/models/decisionTree/algorithm_Ernest/getGainRatio_Ernest.py
+++ src/models/decisionTree/algorithm_Ernest/getGainRatio_Ernest.py
@@ -9,10 +9,11 @@ import API_Ernest
 
 # read csv file
 
-filename = './data/data_Ernest/entireDataSet_Ernest.csv'
+filename = './data/data_Ernest/DataCleanByTed.csv'
 dataSet = pd.read_csv(filename)
 
 ent_attrition = API_Ernest.entD(dataSet, 'Attrition')
+entireAttrSet = API_Ernest.attrSetGenerate(dataSet)
 
 
 #gain_department = gainD(dataSet, 'Department', ent_attrition, 'Attrition')
@@ -21,14 +22,14 @@ ent_attrition = API_Ernest.entD(dataSet, 'Attrition')
 #compute every feature and get best one
 gain = 0
 gain_ratio = 0
-
+gainDic = {}
 for index in dataSet.columns :
     if index != 'Attrition':
 
-        gainDic_feature = API_Ernest.getGainDicByFeature(dataSet, index, ent_attrition, 'Attrition')
+        gainDic_feature = API_Ernest.getGainDicByFeature(dataSet, index, ent_attrition, 'Attrition', entireAttrSet)
 
         gain_ratio_feature = gainDic_feature['gain'] / gainDic_feature['IV']
-
+        gainDic[index] = [ gainDic_feature['gain'], gain_ratio_feature]
         if gainDic_feature['gain'] > gain :
             gain = gainDic_feature['gain']
             bestFeature = index
@@ -37,8 +38,11 @@ for index in dataSet.columns :
             gain_ratio = gain_ratio_feature
             bestRatioFeature = index
 
+df = pd.DataFrame(gainDic,index=['gain','gain ratio'])
 print('Best feature by gain is' ,bestFeature)
 print('Its gain is ' ,gain)
 print('Best feature by gain_ratio is' ,bestRatioFeature)
 print('Its gain_ratio is ' ,gain_ratio)
 
+
+df.to_csv('./reference/InformationGain.csv', index=False)
